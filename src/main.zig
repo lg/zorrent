fad14dfc91695c454940c42060e0eeaca9e80829
Larry Gadea <trivex@gmail.com>
2022-10-09 21:01:04 -0700

    switch to direct array manipulation versus the filereader, way faster

--- src/main.zig
+++ src/main.zig
@@ -1,46 +1,46 @@
 const std = @import("std");
 
-const Errors = error{ BadTorrentFileStart, FieldSizeTooLarge, FieldSizeEmpty, UnexpectedlyShortValue, UnknownType };
+const Errors = error{ UnknownType, BadStringLength };
 
 pub fn main() !void {
-    var heap_allocator = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer heap_allocator.deinit();
-    //var heap_allocator = std.heap.GeneralPurposeAllocator(.{}){};
-    //defer std.debug.assert(!heap_allocator.deinit());
+    // var heap_allocator = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    // defer heap_allocator.deinit();
+    var heap_allocator = std.heap.GeneralPurposeAllocator(.{}){}; // so we can check for memory leaks
+    defer std.debug.assert(!heap_allocator.deinit());
     const allocator = heap_allocator.allocator();
 
     const file = try std.fs.cwd().openFile("ubuntu.torrent", .{});
     defer file.close();
-    const reader = file.reader();
+
+    const file_stat = try file.stat();
+    const data = try file.readToEndAlloc(allocator, file_stat.size);
+    defer allocator.free(data);
 
     const t = std.time.nanoTimestamp();
-    try parse(allocator, reader);
-
+    try parse(allocator, data);
     std.debug.print("total timez: {}\n", .{std.time.nanoTimestamp() - t});
 }
 
-fn parse(allocator: std.mem.Allocator, reader: std.fs.File.Reader) !void {
-    while (true) {
-        const first = reader.readByte() catch return;
-        switch (first) {
+fn parse(_: std.mem.Allocator, data: []const u8) !void {
+    var index: usize = 0;
+
+    while (index < data.len) : (index += 1) {
+        const curChar = data[index];
+
+        switch (curChar) {
             '0'...'9' => { // 4:abcd
-                const size_str_second_part = try reader.readUntilDelimiterAlloc(allocator, ':', 10);
-                defer allocator.free(size_str_second_part);
+                const colon_index = std.mem.indexOfPos(u8, data, index, &[_]u8{':'}) orelse return Errors.BadStringLength;
+                const size_str = data[index..colon_index];
+                const size: u32 = try std.fmt.parseUnsigned(u32, size_str, 10);
+                index = colon_index + size;
+                const value = data[(colon_index + 1) .. index + 1];
 
-                const size_str = try std.mem.concat(allocator, u8, &[_][]u8{ &[_]u8{first}, size_str_second_part });
-                defer allocator.free(size_str);
-
-                const size = try std.fmt.parseUnsigned(u32, size_str, 10);
-                const value_buffer = try allocator.alloc(u8, size);
-                defer allocator.free(value_buffer);
-                const value_size = try reader.readAll(value_buffer); // reads up to the size of the buffer
-                if (value_size != size) return Errors.UnexpectedlyShortValue;
-
-                if (size <= 50) std.debug.print("{s}\n", .{value_buffer}) else std.debug.print("[text of length {}]\n", .{size});
+                if (size <= 50) std.debug.print("{s}\n", .{value}) else std.debug.print("[text of length {}]\n", .{size});
             },
-            'i' => {
-                const num_str = try reader.readUntilDelimiterAlloc(allocator, 'e', 20);
-                defer allocator.free(num_str);
+            'i' => { // i12345e
+                const end_index = std.mem.indexOfPos(u8, data, index, &[_]u8{'e'}) orelse return Errors.BadStringLength;
+                const num_str = data[index + 1 .. end_index];
+                index = end_index;
                 std.debug.print("{s}\n", .{num_str});
             },
             'd' => {
@@ -53,19 +53,9 @@ fn parse(allocator: std.mem.Allocator, reader: std.fs.File.Reader) !void {
                 std.debug.print("<end>\n", .{});
             },
             else => {
-                std.debug.print("unknown type: {c}\n", .{first});
-                unreachable;
+                std.debug.print("unknown type: {c}\n", .{curChar});
+                return Errors.UnknownType;
             },
         }
     }
-}
-
-// var i: usize = 0;
-// while (i < indent) : (i += 1) std.debug.print(" ", .{});
-// std.debug.print("<start dictionary>\n", .{});
-// indent += 2;
-
-test "basic test" {
-    @breakpoint();
-    try std.testing.expectEqual(10, 3 + 7);
 }
